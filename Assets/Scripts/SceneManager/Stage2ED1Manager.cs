43d426e128e1bd542cb3d16c7b1b527a731437c6
Gandalf925 <theengineerofoz@gmail.com>
2023-07-05 15:16:11 +0900

    Update/Stage2ED1Completed

--- Assets/Scripts/SceneManager/Stage2ED1Manager.cs
+++ Assets/Scripts/SceneManager/Stage2ED1Manager.cs
@@ -11,7 +11,10 @@ public class Stage2ED1Manager : MonoBehaviour
     public Transform pepeStopPos1;
     public Transform pepeStopPos2;
     public GameObject box;
+    public Transform boxMovePath1;
+    public Transform boxMovePath2;
     public GameObject egg;
+    public GameObject shinyItemEffect;
     public GameObject explosion;
     public Transform explosionPos;
     public GameObject bullet;
@@ -43,14 +46,54 @@ public class Stage2ED1Manager : MonoBehaviour
         yield return new WaitForSecondsRealtime(2.5f);
 
         player.transform.DOPunchScale(new Vector3(30f, 30f, 0), 1f);
-        bullet = Instantiate(bullet, player.transform.position, Quaternion.identity);
+        GameObject pepeBullet1 = Instantiate(bullet, player.transform.position, Quaternion.identity);
 
 
-        yield return new WaitForSecondsRealtime(0.5f);
+        yield return new WaitForSecondsRealtime(0.3f);
 
-        explosion = Instantiate(explosion, explosionPos.transform.position, Quaternion.identity);
+        GameObject explosion1 = Instantiate(explosion, explosionPos.transform.position, Quaternion.identity);
+        Destroy(pepeBullet1);
 
-        // box.transform.DOPath()
+        box.transform.DOLocalPath(
+            new[]{
+            boxMovePath1.position,
+            boxMovePath2.position,
+        }, 0.5f, PathType.Linear);
+        box.transform.DORotate(new Vector3(0f, 0f, 5f), 0.5f);
+
+        yield return new WaitForSeconds(0.3f);
+
+        GameObject shinyEgg = Instantiate(shinyItemEffect, egg.transform.position, Quaternion.identity);
+
+        yield return new WaitForSeconds(1f);
+
+        player.transform.DOShakePosition(2f, 10f, 30, 1, false, false);
+
+        yield return new WaitForSeconds(2.5f);
+
+        player.transform.DOPunchScale(new Vector3(30f, 30f, 0), 1f);
+        GameObject pepeBullet2 = Instantiate(bullet, player.transform.position, Quaternion.identity);
+
+        yield return new WaitForSeconds(0.3f);
+
+        GameObject explosion2 = Instantiate(explosion, explosionPos.transform.position, Quaternion.identity);
+        Destroy(shinyEgg);
+        Destroy(pepeBullet2);
+
+        egg.transform.DOLocalPath(
+            new[]{
+                boxMovePath1.position,
+                boxMovePath2.position,
+            }, 0.5f, PathType.Linear);
+
+        yield return new WaitForSeconds(1.3f);
+
+
+        player.transform.DOShakeScale(1.3f, 100f, 60, 90f, false);
+
+        yield return new WaitForSeconds(1.3f);
+
+        player.transform.DOMove(pepeStopPos2.transform.position, 0.3f);
 
         // StartCoroutine(SkipScene());
     }
