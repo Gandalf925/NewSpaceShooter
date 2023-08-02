fc77124343e24d1d18fa2d3f24a073867d7dab49
Gandalf925 <theengineerofoz@gmail.com>
2023-08-03 07:08:37 +0900

    Update/AddPepes

--- Assets/Scripts/SceneManager/Stage4Manager.cs
+++ Assets/Scripts/SceneManager/Stage4Manager.cs
@@ -1,18 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class Stage4Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    GameObject player;
+    PlayerController playerController;
+    [Header("Elites")]
+    public GameObject magicianPepePrefab;
+    public GameObject crownPepePrefab;
+
+    [Header("Boss")]
+    public GameObject bossPrefab;
+    public Transform bossStartPos;
+    public Transform bossStopPos;
+
+    [Header("Normal Enemies")]
+    public GameObject[] normalEnemies;
+    public Transform NormalStartPos1;
+    public Transform NormalStartPos2;
+    public Transform NormalStartPos3;
+
+    [Header("StartTextFrame")]
+    [SerializeField] GameObject startTextFrame;
+    [SerializeField] Transform frameStartPos;
+    [SerializeField] Transform frameStopPos;
+    [SerializeField] Transform frameEndPos;
+
+    [Header("Manager")]
+    GameManager gameManager;
+    UIManager uIManager;
+
+    public AudioClip stage4BGM;
+    public AudioClip stage4BossBGM;
+
+
+    private void Start()
     {
-        
+        player = FindObjectOfType<PlayerController>().gameObject;
+        playerController = player.GetComponent<PlayerController>();
+        uIManager = FindObjectOfType<UIManager>();
+        startTextFrame.transform.position = frameStartPos.position;
+
+        uIManager.FadeIn();
+        // BGMManager.instance.PlayBGM(stage3BGM);
+        StartCoroutine(StartFrameIn());
+
+        StartCoroutine(StageStart());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator StageStart()
     {
-        
+        yield return new WaitForSeconds(6f);
+        Debug.Log("Start Stage4");
+    }
+
+    IEnumerator StartFrameIn()
+    {
+        yield return new WaitForSecondsRealtime(1.5f);
+
+        startTextFrame.transform.DOMove(frameStopPos.position, 0.5f);
+
+        yield return new WaitForSecondsRealtime(2f);
+
+        startTextFrame.transform.DOMove(frameEndPos.position, 0.5f);
+
+        yield return new WaitForSecondsRealtime(1f);
     }
 }
