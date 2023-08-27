709e6cdae0b4be759aeef027ed2e2fdfb5f37b47
Gandalf925 <theengineerofoz@gmail.com>
2023-08-28 07:08:15 +0900

    Update/PlayerMoveFix

--- Assets/Scripts/Player/Player3DBulletController.cs
+++ Assets/Scripts/Player/Player3DBulletController.cs
@@ -2,33 +2,44 @@ using UnityEngine;
 
 public class Player3DBulletController : MonoBehaviour
 {
-    public GameObject bulletPrefab;  // BulletPrefabをインスペクターからアタッチ
-    public float shootSpeedZ = 20.0f;  // 弾のZ軸方向の発射速度
-    public float shootSpeedY = 5.0f;  // 弾のY軸方向の発射速度（インスペクターで調整可能）
+    public GameObject bulletPrefab;
+    public float shootSpeedZ = 20.0f;
+    public float shootSpeedY = 5.0f;
+    public float fireRate = 1.0f;  // 1秒あたりの発射回数
+
+    [SerializeField] GameObject explosionPrefab;
+
+    private float nextFire = 0.0f;  // 次に弾を発射できるタイミング
 
     // Update is called once per frame
     void Update()
     {
-        // ここではSpaceキーを押すと発射するようにしています。
-        if (Input.GetKeyDown(KeyCode.Space))
+        // 現在の時間が次の発射時間以上かつ、マウス左クリックが押された場合に発射
+        if (Time.time > nextFire && Input.GetKey(KeyCode.Mouse0))
         {
+            nextFire = Time.time + 1.0f / fireRate;  // 次に発射できる時間を更新
             ShootBullet();
         }
     }
 
     void ShootBullet()
     {
-        // BulletPrefabから新しい弾をインスタンス化
         GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-
-        // 発射方向を計算（Z軸方向に移動しながらY軸方向に少し上へ）
         Vector3 shootDirection = new Vector3(0.0f, shootSpeedY, shootSpeedZ);
-
-        // 弾に速度を与える
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
         if (rb != null)
         {
             rb.velocity = transform.TransformDirection(shootDirection);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy") || collision.CompareTag("Boss"))
+        {
+            GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(explosion, 0.5f);
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Scripts/Player/Player3DController.cs
+++ Assets/Scripts/Player/Player3DController.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class Player3DController : MonoBehaviour
 {
     public float speed = 5.0f; // 移動速度
-    public float stopDistance = 0.1f; // この距離以下になったら停止
-    private Rigidbody rb; // Rigidbodyコンポーネント
+    public float touchSensitivity = 0.1f; // タッチ感度（移動を開始するための最小距離）
+    private Rigidbody rb; // Rigidbody コンポーネント
     private float fixedZPosition; // 固定されたZ座標
 
     void Start()
@@ -27,20 +27,27 @@ public class Player3DController : MonoBehaviour
         }
 
         Vector3 mouseScreenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10); // Z値はカメラからの距離
-
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
         mouseWorldPos.z = fixedZPosition; // Z座標も固定
 
-        Vector3 targetPosition = mouseWorldPos + new Vector3(1, 1, 0);
-        Vector3 diff = targetPosition - transform.position;
+        // オフセットを加えて目標位置を計算
+        float offsetX = 4f;
+        float offsetY = 0.3f;
+        Vector3 targetPosition = mouseWorldPos + new Vector3(offsetX, offsetY, 0);
 
-        if (diff.magnitude < stopDistance)
+        // 現在位置と目標位置との差分を計算
+        Vector3 diff = targetPosition - transform.position;
+        diff.z = 0; // Z座標の差分は0に
+
+        // 差分ベクトルが一定値以上なら移動、それ以下なら停止
+        if (diff.magnitude > touchSensitivity)
         {
-            rb.velocity = Vector3.zero;
+            rb.velocity = diff.normalized * speed;
         }
         else
         {
-            rb.velocity = diff.normalized * speed;
+            transform.position = targetPosition; // 目標位置に移動
+            rb.velocity = Vector3.zero; // 速度をゼロに
         }
     }
 }
